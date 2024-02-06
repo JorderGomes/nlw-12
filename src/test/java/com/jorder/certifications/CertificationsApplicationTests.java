d5118588ea6f2a045a057fbdfb49a227248ed274
JorderGomes <jordernoveau@gmail.com>
2024-02-06 15:37:25 -0300

    #master - initial commit

---
+++ src/test/java/com/jorder/certifications/CertificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorder.certifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
